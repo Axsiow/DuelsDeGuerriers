33aeabaf5baa458430e5d671de89df5f4fd36d9d
Axsiow <TehSiow2@gmail.com>
2021-04-13 16:25:37 +0200

    add attac

--- src/Guerrier.java
+++ src/Guerrier.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Guerrier {
 
     private String nom;
@@ -14,8 +16,31 @@ public class Guerrier {
         return pv;
     }
 
+    public void setPv(int pv) {
+        if (pv > 100) {
+            this.pv = 100;
+        }   else if (pv < 0){
+            this.pv = 0;
+        } else {
+            this.pv = pv;
+        }
+    }
+
     public void afficherInfos() {
         System.out.println(this.nom+"{PV="+this+"}");
     }
 
+    public void attaquer (Guerrier Adversaire) {
+        for (int i=0; i<nbDesAttaque; i++) {
+            Random rd = new Random();
+            int valeurLancer = rd.nextInt(bound:)+1;
+            Adversaire.subirDegats(valeurLancer);
+            Adversaire.afficherInfos();
+        }
+
+    }
+
+    public void subirDegats(int degats) {
+        this.pv = this.pv - degats
+    }
 }
