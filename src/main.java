e46d0fcbced766a59ebfdb2d6eeb4a7b15b045ab
Axsiow <TehSiow2@gmail.com>
2021-05-01 14:17:38 +0200

    c'est de la merde

--- src/main.java
+++ src/main.java
@@ -1,23 +1,44 @@
+import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Guerrier g1 = new Guerrier("Jay Ksaaffer", 35, 3);
-        Guerrier g2 = new Guerrier("Morph Hal", 30, 4);
-        Nain n1 = new Nain("Mardézelf", 25, 2, 4);
-        Nain n2 = new Nain("Hildegarde Havou", 12, 4, 4);
-        Elfe e1 = new Elfe("Amaury Oley", 30, 2, 6);
-        Elfe e2 = new Elfe("Marie Houana",35, 2, 5);
+		Guerrier g1 = new Guerrier("Jay Ksaaffer", 35, 3);
+		Guerrier g2 = new Guerrier("Morph Hal", 30, 4);
+		Nain n1 = new Nain("Mardézelf", 25, 2, 4);
+		Nain n2 = new Nain("Hildegarde Havou", 12, 4, 4);
+		Elfe e1 = new Elfe("Amaury Oley", 30, 2, 6);
+		Elfe e2 = new Elfe("Marie Houana",35, 2, 5);
 
-        list<Guerrier> combattants= new ArrayList<>();
-        g2.afficherInfos();
-        GuerriersUtil.regenereGuerrier(g2);
-        g2.afficherInfos();
-        GuerriersUtil.regenereGuerrier(g2);
-        g2.afficherInfos();
-        g2.afficherInfos();
-    }
+//        list<Guerrier> combattants= new ArrayList<>();
+		g2.afficherInfos();
+		GuerriersUtil.regenereGuerrier(g2);
+		g2.afficherInfos();
+		GuerriersUtil.regenereGuerrier(g2);
+		g2.afficherInfos();
+		g2.afficherInfos();
+
+		// create listes combatants
+		ArrayList<Guerrier> fighterBot = new ArrayList<Guerrier>();
+		ArrayList<Guerrier> fighterPlayer = new ArrayList<Guerrier>();
+
+		// Filling listes
+		int guerriersMax = guerriers.size();
+
+		for (int i=0; i<guerriersMax; i++) {
+			Random random = new Random();
+			int choiceBot = random.nextInt(guerriers.size());
+			System.out.println(choiceBot);
+			fighterBot.add(guerriers.get(choiceBot));
+			System.out.println(guerriers.get(choiceBot).nom+" a rejoint l'équipe adverse");
+			guerriers.remove(choiceBot);
+
+			// Display remaining fighters
+			ShowFighters(guerriers);
+
+		}
+	}
 }
